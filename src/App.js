58627121870c75323df5ca6db22932450a1b0404
Toortle <83099906+aidenhock@users.noreply.github.com>
2024-04-04 15:56:02 -0700

    init security check

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { initializeApp } from "firebase/app";
 
 // const firebaseConfig = {
@@ -10,35 +10,43 @@ import { initializeApp } from "firebase/app";
 //   appId: "1:1093431620967:web:e6742b436279bbf107ebc2",
 //   measurementId: "G-1C78KFH4GP"
 // };
-import React from 'react';
-import SearchBarComponent from './components/SearchBarComponent';
-import LocationsList from './components/LocationsList';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
+
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NavbarComponent from './components/NavbarComponent';
 import HomePage from './pages/HomePage';
 import LocationPage from './pages/LocationPage';
 import AboutPage from './pages/AboutPage';
+import SecurityPage from './security/SecurityPage';
+import { isSessionValid } from './security/SessionManager';
 
 const App = () => {
-  // Function for handling search will go here
-  const handleSearch = searchTerm => {
-    console.log(`Searching for: ${searchTerm}`);
-    // Implement your search logic or update state with the search term
+  const [isAuthenticated, setIsAuthenticated] = useState(isSessionValid());
+
+  useEffect(() => {
+    setIsAuthenticated(isSessionValid());
+  }, []);
+
+  const handleAuthenticationSuccess = () => {
+    setIsAuthenticated(true);
   };
+
+  if (!isAuthenticated) {
+    return <SecurityPage onAuthenticated={handleAuthenticationSuccess} />;
+  }
 
   return (
     <Router>
       <NavbarComponent />
       <Routes>
-        <Route path="/" element={<HomePage onSearch={handleSearch} />} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/locations/:id" element={<LocationPage />} />
         <Route path="/about" element={<AboutPage />} />
-        {/* Add other routes here as needed */}
       </Routes>
     </Router>
   );
 };
 
 export default App;
+
