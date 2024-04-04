546e6ac705c884be60f59bb879b110a275b6b230
mai <157174437+natmai@users.noreply.github.com>
2024-04-04 13:59:43 -0700

    rhinos location -> rhino locations

--- src/components/NavbarComponent.js
+++ src/components/NavbarComponent.js
@@ -6,7 +6,7 @@ const NavbarComponent = () => {
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">RHINOS LOCATION</Navbar.Brand>
+        <Navbar.Brand href="#home">RHINO LOCATIONS</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
