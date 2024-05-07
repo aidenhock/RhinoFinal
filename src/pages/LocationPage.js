1b610db2bdb6061f23d4c59c2cf2bfefbb451460
mai <157174437+natmai@users.noreply.github.com>
2024-05-07 11:43:08 -0700

    added image gallery package

--- src/pages/LocationPage.js
+++ src/pages/LocationPage.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { doc, getDoc } from 'firebase/firestore';
-import { Button, Form, Container, Row, Col, InputGroup } from 'react-bootstrap';
 import { db } from '../firebase'; // Ensure this path is correct
 import './LocationPage.css'; // Make sure to include the CSS file
+import ImageGallery from 'react-image-gallery'; // Import the ImageGallery component
+import 'react-image-gallery/styles/css/image-gallery.css';
+
 
 const LocationPage = () => {
   let { id } = useParams(); // This hook gives us access to the URL parameters
@@ -16,7 +18,6 @@ const LocationPage = () => {
         const docSnap = await getDoc(docRef);
 
         if (docSnap.exists()) {
-          // Extract data and update state
           setLocationData({
             id: docSnap.id,
             ...docSnap.data(),
@@ -32,35 +33,32 @@ const LocationPage = () => {
     fetchLocationData();
   }, [id]); // Re-run the effect if the URL parameter `id` changes
 
-  // If data has not been fetched yet, you can show a loading spinner or a placeholder
   if (!locationData) {
     return <div>Loading...</div>;
   }
+
+  // Convert images to the format expected by react-image-gallery
+  const images = locationData.pictures.map((image) => ({
+    original: image,
+    thumbnail: image // You can use the same URL if you don't have separate thumbnails
+  }));
+
   return (
     <div className="location-page">
       <div className="location-header">
-      <Col className="text-center">
-          <h2 className="location-title">{locationData.title}</h2>
-        </Col>
-              </div>
-      {locationData.pictures.map((image, index) => (
-        <img key={index} src={image} alt={`${locationData.title}`} className="location-image" />
-      ))}
+        <h2 className="location-title">{locationData.title}</h2>
+      </div>
+      <ImageGallery items={images} />
       <div className="location-content">
-      <div className="location-tags">
+        <div className="location-tags">
           {locationData.tags.map((tag, index) => (
-            <span key={index} className="tag-label">
-              {tag}
-            </span>
+            <span key={index} className="tag-label">{tag}</span>
           ))}
         </div>
         <p className="location-description">{locationData.description}</p>
-        {/* Render tags as labels */}
- 
       </div>
     </div>
   );
-  
 };
 
 export default LocationPage;
